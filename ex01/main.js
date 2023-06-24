c19e1ef1d3b4136bdddae708b02b19650c367a9e
heltonsantos001 <heltonsanto001@gmail.com>
2023-06-24 00:30:43 -0300

    atualizei

---
+++ ex01/main.js
@@ -0,0 +1,25 @@
+const message = 'hellowworld';
+const message2 = 'ola mundo';
+const nome = 'helton';
+
+console.log('meu primeiro '+ message);
+console.log(`meu primeiro ${message2.toUpperCase()} ${message2.toLowerCase()}`);
+
+console.log(nome.split(''));
+
+const number = 5;
+
+console.log(typeof number.toString());
+
+false
+true
+
+console.log(2 == 2)
+
+//Null & underfined
+
+const x = null
+
+const y = undefined
+
+console.log(y)
