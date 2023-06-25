8f1905cdda63ecb381a8b4c1d0cf8a6132a2ad56
heltonsantos001 <heltonsanto001@gmail.com>
2023-06-25 12:06:59 -0300

    atualizar

---
+++ ex02/main.js
@@ -0,0 +1,14 @@
+const cavalo = ['pata', 'olho', 'chifre', 10, false];
+
+
+
+const oi = true
+
+if (oi == true) {
+   cavalo.pop()
+   cavalo.pop()
+   cavalo.unshift('orelha')
+   cavalo.push('nobru')
+   console.log(cavalo.indexOf('orelha'))
+ 
+}
